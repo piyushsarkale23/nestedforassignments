a02ff819f2187a909b8048e4f7ee9ba39712331d
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-01-17 16:29:04 +0530

    Add project files.

---
+++ nestedforr/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nestedforr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// outer for loop which can be used for no. rows
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++) // column
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("---------------");
+
+
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++) // column
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("-----------------");
+
+
+
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++) // column
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("------------------");
+
+            
+
+            for (int i = 1; i <=5 ; i++)
+            {
+                for (int j = 5; j >= i; j--) // column
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            */
+            int k = 5;
+            for(int i = 1;i<=5;i++)
+            {
+                if(k>=1)
+                {
+                    Console.WriteLine("*");
+                    k--;
+                }
+                for(int j=5;j<=k;j++)
+                {
+                    Console.WriteLine("  ");
+                }
+            }
+
+            }
+        }
+    }
